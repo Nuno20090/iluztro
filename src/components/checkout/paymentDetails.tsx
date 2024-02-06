20716b0337a9aca0fd934b5b6656e80a5d8d052c
Nuno Costa <nuno20090@gmail.com>
2024-02-06 21:32:21 +0000

    Payment details real info

--- src/components/checkout/paymentDetails.tsx
+++ src/components/checkout/paymentDetails.tsx
@@ -1,16 +1,33 @@
+import { useEffect, useState } from "react";
 import { ICartItem } from "../../dataDefinitions/cartItem";
-import { BuyerLocation } from "../../library/order";
+import { BuyerLocation, Order } from "../../library/order";
 
 interface PaymentDetailsParams {
     cartItems: ICartItem[];
     buyerLocation: BuyerLocation;
 }
 
-
 export function PaymentDetails({
     cartItems,
     buyerLocation
 }: PaymentDetailsParams) {
+
+    const [itemsCost, setItemsCost] = useState<number>(0);
+    const [shippingCost, setShippingCost] = useState<number>(0);
+    const [totalCost, setTotalCost] = useState<number>(0);
+
+    useEffect(() => {
+
+        const itemsCost = Order.getOrderItemsTotal(cartItems);
+        const shippingCost = Order.getOrderDeliveryCosts(cartItems, buyerLocation);
+        const totalCost = itemsCost + shippingCost;
+
+        setItemsCost(itemsCost);
+        setShippingCost(shippingCost);
+        setTotalCost(totalCost);
+
+    }, [cartItems, buyerLocation]);
+
     return <div
         className="payment-details">
         <table>
@@ -21,15 +38,15 @@ export function PaymentDetails({
                 </tr>
                 <tr>
                     <td>Items Cost</td>
-                    <td>30€</td>
+                    <td>{itemsCost}€</td>
                 </tr>
                 <tr>
                     <td>Shipping Cost</td>
-                    <td>20€</td>
+                    <td>{shippingCost}€</td>
                 </tr>
                 <tr>
                     <td>Total</td>
-                    <td>50€</td>
+                    <td>{totalCost}€</td>
                 </tr>
             </tbody>
         </table>
